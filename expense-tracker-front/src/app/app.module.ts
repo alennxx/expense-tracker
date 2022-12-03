f4a7ba9b67cef0816037d09c11c3f0d701ed7764
Natalia <natalia-gasiorowska@o2.pl>
2022-12-03 21:44:39 +0100

    added routing, basic expenses table

--- expense-tracker-front/src/app/app.module.ts
+++ expense-tracker-front/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ExpenseDetailsComponent } from './expense/components/expense-details/expense-details.component';
 import { ExpensesOverviewComponent } from './expense/components/expenses-overview/expenses-overview.component';
+import { ExpenseModule } from './expense/expense.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExpenseDetailsComponent,
-    ExpensesOverviewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    RouterModule.forRoot([
+      { path: '', redirectTo: "/expenses", pathMatch: 'full' },
+      {
+        path: 'expenses',
+        component: ExpensesOverviewComponent
+      }
+    ]),
+    ExpenseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- expense-tracker-front/src/app/expense/components/expenses-overview/expenses-overview.component.ts
+++ expense-tracker-front/src/app/expense/components/expenses-overview/expenses-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Expense } from '../../model/expense';
 
 @Component({
   selector: 'app-expenses-overview',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./expenses-overview.component.scss']
 })
 export class ExpensesOverviewComponent {
+
+  expenses: Expense[] = [
+    {
+      id: 1,
+      name: 'Catnip',
+      amount: 10.0,
+      category: 'cat'
+    },
+    {
+      id: 2,
+      name: 'Bread and cheese',
+      amount: 13.46,
+      category: 'food'
+    },
+    {
+      id: 3,
+      name: 'Chocolate',
+      amount: 4.99,
+      category: 'food'
+    },
+    {
+      id: 4,
+      name: 'Candles',
+      amount: 89.99,
+      category: 'home'
+    },
+    {
+      id: 5,
+      name: 'Winter gloves',
+      amount: 54.99,
+      category: 'clothes'
+    },
+  ];
 
 }
